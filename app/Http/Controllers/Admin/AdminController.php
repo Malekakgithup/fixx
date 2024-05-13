cc6242baebf146ac3c5a77d3bcdcc597412ae5f1
malekakgithup <malekak.ai.0.h@gmail.com>
2024-05-14 00:27:28 +0300

    done

--- app/Http/Controllers/Admin/AdminController.php
+++ app/Http/Controllers/Admin/AdminController.php
@@ -16,34 +16,6 @@ use SimpleSoftwareIO\QrCode\Facades\QrCode;
 class AdminController extends Controller
 {
 
-    public function AddElectrical(Request $request)
-    {
-        $validator = Validator::make($request->all(), [
-            'name' => 'required|string',
-            'size' => 'required|integer',
-            'warning' => 'required|string',
-            'notes' => 'nullable|string',
-            'way_of_work' => 'nullable|string',
-        ]);
-        if ($validator->fails()) {
 
-            return response()->json($validator->errors(), 422);
-        }
-        $item = Electrical_parts::create([
-            'name' => $request->input('name'),
-            'size' => $request->input('size'),
-            'warning' => $request->input('warning'),
-            'notes' => $request->input('notes'),
-            'way_of_work' => $request->input('way_of_work'),
-        ]);
-
-        $qrCode = QrCode::size(200)->generate($item->id);
-
-        return response()->json([
-            'message' => 'تم إنشاء السجل بنجاح',
-            'item' => $item,
-            'qr_code' => base64_encode($qrCode),
-        ], 201);
-    }
 
 }
